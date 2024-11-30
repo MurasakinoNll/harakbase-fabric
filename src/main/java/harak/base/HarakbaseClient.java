bb6d5e83321abb4433cbfb9cae62db82b5ce6485
MurasakinoNll <meekacloneacc@gmail.com>
2024-11-30 04:12:46 +0300

    first commit

---
+++ src/main/java/harak/base/HarakbaseClient.java
@@ -0,0 +1,11 @@
+package harak.base;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HarakbaseClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/harak/base/HarakbaseDataGenerator.java
@@ -0,0 +1,11 @@
+package harak.base;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HarakbaseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
